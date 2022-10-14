6f28839c94009256b96b9d1f0f66e325bc0d0ea8
Dan Bially Levy <dbiallyl@macalester.edu>
2022-10-13 20:40:08 -0500

    Got enemy moving!!!!

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Coded with help from: https://www.youtube.com/watch?v=KoFDDp5W5p0
+
 public class EnemyScript : MonoBehaviour
 {
+    public Transform[] waypoints;
+    public float speed = 0.02f;
+    int waypointIndex = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[0].position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (waypointIndex >= waypoints.Length) {
+            waypointIndex = 0;
+        }
+        Vector3 target = waypoints[waypointIndex].position;
+
+        transform.position = Vector2.MoveTowards(transform.position, target, (speed * Time.deltaTime));
+        print("target: " + target + " actual: " + transform.position);
+        if (transform.position.x == target.x && transform.position.y == target.y) {
+            print(waypointIndex);
+            waypointIndex++;
+        }
         // if not frozen
         // continue along walk path
         // check if can see player
@@ -25,6 +41,10 @@ public class EnemyScript : MonoBehaviour
         if (collision.collider.CompareTag("Purifier")) {
             // Freeze enemy (?)
             // Maybe start timer for purification
+        }
+        if (collision.collider.CompareTag("Waypoint")) {
+            // print("happening");
+            // waypointIndex++;
         }
     }
 }
