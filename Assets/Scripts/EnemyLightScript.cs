79834d0504a135520f25220e471919011b42bd2a
Dan Bially Levy <dbiallyl@macalester.edu>
2022-10-27 09:57:52 -0500

    Added rigidbody to enemylight object

--- Assets/Scripts/EnemyLightScript.cs
+++ Assets/Scripts/EnemyLightScript.cs
@@ -16,12 +16,19 @@ public class EnemyLightScript : MonoBehaviour
         
     }
 
-    // NOT WORKING
-    void OnCollisionEnter2D(Collision2D collision){
+    void onTriggerEnter(Collider collider) {
         print("Baller 1");
-        if (collision.collider.CompareTag("Player")) {
+        if (collider.CompareTag("Player")) {
             print("Baller player");
         }
-
     }
+
+    // // NOT WORKING
+    // void OnCollisionEnter2D(Collision2D collision){
+    //     print("Baller 1");
+    //     if (collision.collider.CompareTag("Player")) {
+    //         print("Baller player");
+    //     }
+
+    // }
 }
