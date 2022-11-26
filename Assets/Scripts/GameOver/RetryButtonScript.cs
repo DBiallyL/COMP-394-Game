2124f91f15ba0d2acb74c07380935871b66ae345
Dan Bially Levy <dbiallyl@macalester.edu>
2022-11-26 11:26:11 -0600

    Finished implementing return button

---
+++ Assets/Scripts/GameOver/RetryButtonScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RetryButtonScript : MonoBehaviour
+{
+    Text text;
+    Image sprite;
+    public Sprite inactiveSprite;
+    public Sprite activeSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**
+    * Changes to the inactive button sprite
+    */
+    public void InactivateButton() {
+        sprite.sprite = inactiveSprite;
+    }
+
+    /**
+    * Changes to the active button sprite
+    */
+    public void ActivateButton() {
+        sprite.sprite = activeSprite;
+    }
+}
